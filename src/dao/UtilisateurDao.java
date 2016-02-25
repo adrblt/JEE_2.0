22034d9646f647e423f31892360d0d3b83bb4995
adrien <adri.b1@hotmail.fr>
2016-02-25 15:41:20 +0100

    first commit

---
+++ src/dao/UtilisateurDao.java
@@ -0,0 +1,51 @@
+package dao;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import entities.Utilisateur;
+
+@Stateless
+public class UtilisateurDao {
+    private static final String JPQL_SELECT_PAR_EMAIL = "SELECT u FROM Utilisateur u WHERE u.email=:email";
+    private static final String PARAM_EMAIL           = "email";
+    
+    // Enregistrement d'un nouvel utilisateur
+    public void creer( Utilisateur utilisateur ) throws DAOException {
+        try {
+        	EntityManagerFactory emf = Persistence.createEntityManagerFactory("Test");
+        	EntityManager em = emf.createEntityManager();
+            em.getTransaction().begin();
+            em.persist( utilisateur );
+            em.flush();
+            em.close();
+            emf.close();
+            //em.getTransaction().commit();
+        } catch ( Exception e ) {
+            throw new DAOException( e );
+        }
+    }
+
+    // Recherche d'un utilisateur à partir de son adresse email
+    public Utilisateur trouver( String email ) throws DAOException {
+        Utilisateur utilisateur = null;
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("Test");
+    	EntityManager em = emf.createEntityManager();
+        Query requete = em.createQuery( JPQL_SELECT_PAR_EMAIL );
+        requete.setParameter( PARAM_EMAIL, email );
+        try {
+            utilisateur = (Utilisateur) requete.getSingleResult();
+        } catch ( NoResultException e ) {
+            return null;
+        } catch ( Exception e ) {
+            throw new DAOException( e );
+        }
+        return utilisateur;
+    }
+}
---
+++ src/entities/Utilisateur.java
@@ -0,0 +1,90 @@
+package entities;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import java.util.Date;
+
+
+@Entity
+public class Utilisateur implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+	private int idUtilisateur;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dateInscription;
+	
+	@NotNull( message = "Veuillez saisir une adresse email" )
+	@Pattern( regexp = "([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)", message = "Merci de saisir une adresse mail valide" )
+	private String email;
+
+	private int idRole;
+
+	@NotNull( message = "Veuillez saisir un login" )
+	@Size( min = 3, message = "Le login doit contenir au moins 3 caractères" )
+	private String login;
+	
+	@NotNull( message = "Veuillez saisir un mot de passe" )
+	@Size( min = 3, message = "Le mot de passe doit contenir au moins 3 caractères" )
+	private String mdp;
+	//@Pattern(regexp = ".*(?=.{8,})(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).*", message = "Le mot de passe saisi n'est pas assez sécurisé")
+
+	public Utilisateur() {
+	}
+
+	public int getIdUtilisateur() {
+		return this.idUtilisateur;
+	}
+
+	public void setIdUtilisateur(int idUtilisateur) {
+		this.idUtilisateur = idUtilisateur;
+	}
+
+	public Date getDateInscription() {
+		return this.dateInscription;
+	}
+
+	public void setDateInscription(Date dateInscription) {
+		this.dateInscription = dateInscription;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getIdRole() {
+		return this.idRole;
+	}
+
+	public void setIdRole(int idRole) {
+		this.idRole = idRole;
+	}
+
+	public String getLogin() {
+		return this.login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getMdp() {
+		return this.mdp;
+	}
+
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+
+}
